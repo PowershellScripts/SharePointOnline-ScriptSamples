402c8ff71aa5fd2b0d01b8e8110a473295681b71
PowershellScripts <42035526+PowershellScripts@users.noreply.github.com>
2019-10-16 23:15:20 +0300

    Add files via upload

---
+++ Lists and Libraries Management/Break inheritance for all SPO lists in a site/SetUniquePermissions for all lists.ps1
@@ -0,0 +1,73 @@
+﻿function Set-SPOListBreakRoleInheritance
+{
+  
+   param (
+        [Parameter(Mandatory=$true,Position=1)]
+		[string]$Username,
+		[Parameter(Mandatory=$true,Position=2)]
+		[string]$Url,
+        [Parameter(Mandatory=$true,Position=3)]
+		[string]$AdminPassword,
+        [Parameter(Mandatory=$false,Position=4)]
+		[bool]$IncludeSubsites=$false
+		)
+  
+  
+  
+   $password = ConvertTo-SecureString -string $AdminPassword -AsPlainText -Force
+  $ctx=New-Object Microsoft.SharePoint.Client.ClientContext($Url)
+  $ctx.Credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($Username, $password)
+  $ctx.Load($ctx.Web.Lists)
+  $ctx.Load($ctx.Web)
+  $ctx.Load($ctx.Web.Webs)
+  $ctx.ExecuteQuery()
+  Write-Host 
+  Write-Host $ctx.Url -BackgroundColor White -ForegroundColor DarkGreen
+  foreach( $ll in $ctx.Web.Lists)
+  {
+            
+        $ll.BreakRoleInheritance($true, $false)
+    $ll.Update()
+
+        try
+        {
+        $ctx.ExecuteQuery()
+        Write-Host "Removed inherited permissions for " $ll.Title
+        }
+        catch
+        {
+        Write-Host "Failed to remove permissions for " $ll.Title
+        }
+
+         
+        
+        }
+
+         
+        
+        
+  
+        
+
+  
+
+  if($ctx.Web.Webs.Count -gt 0 -and $IncludeSubsites)
+  {
+    for($i=0; $i -lt $ctx.Web.Webs.Count ; $i++)
+    {
+        Set-SPOListBreakRoleInheritance -Url ($ctx.Web.Webs[$i].Url) -Username $Username -AdminPassword $AdminPassword -IncludeSubsites $IncludeSubsites
+    }
+
+  }
+  
+  
+  }
+
+
+# Paths to SDK. Please verify location on your computer.
+Add-Type -Path "c:\Program Files\Common Files\microsoft shared\Web Server Extensions\15\ISAPI\Microsoft.SharePoint.Client.dll" 
+Add-Type -Path "c:\Program Files\Common Files\microsoft shared\Web Server Extensions\15\ISAPI\Microsoft.SharePoint.Client.Runtime.dll" 
+
+
+
+Set-SPOListBreakRoleInheritance -Username "trial@trialtrial123.onmicrosoft.com" -Url "https://trialtrial123.sharepoint.com" -AdminPassword "Pass" -IncludeSubsites $false 
