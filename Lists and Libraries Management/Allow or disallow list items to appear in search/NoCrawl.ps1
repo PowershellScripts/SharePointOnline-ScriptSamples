8fe5a22f16f3b78a04dfe2a9a164da1d90319f90
JakubPiegza <45433779+JakubPiegza@users.noreply.github.com>
2020-02-28 01:27:05 +0100

    Update NoCrawl.ps1

--- Lists and Libraries Management/Allow or disallow list items to appear in search/NoCrawl.ps1
+++ Lists and Libraries Management/Allow or disallow list items to appear in search/NoCrawl.ps1
@@ -3,47 +3,38 @@
 # Created by Arleta Wanat, 2015 
 #
 
-function Set-SPOList
-{
-param (
+function Set-SPOList{
+    param (
         [Parameter(Mandatory=$true,Position=0)]
-		[string]$ListName,
+	[string]$ListName,
         [Parameter(Mandatory=$true,Position=1)]
-		[bool]$NoCrawl  
-		)
+	[bool]$NoCrawl  
+	)
 
     $ll=$ctx.Web.Lists.GetByTitle($ListName)
     $ll.NoCrawl = $NoCrawl
     $ll.Update()
 
-    try
-    {
+    try{
         $ctx.ExecuteQuery()
         Write-Host "Done" -ForegroundColor Green
-       }
-
-       catch [Net.WebException] 
-        {
-            
+    }
+    catch [Net.WebException] {
             Write-Host "Failed" $_.Exception.ToString() -ForegroundColor Red
-        }
-
-
+    }
 }
 
 
 
-function Connect-SPOCSOM
-{
- param (
-  [Parameter(Mandatory=$true,Position=1)]
-		[string]$Username,
-		[Parameter(Mandatory=$true,Position=2)]
-		[string]$AdminPassword,
+function Connect-SPOCSOM{
+    param (
+        [Parameter(Mandatory=$true,Position=1)]
+	[string]$Username,
+	[Parameter(Mandatory=$true,Position=2)]
+	[string]$AdminPassword,
         [Parameter(Mandatory=$true,Position=2)]
-		[string]$Url
-)
-
+	[string]$Url
+    )
 
   $password = ConvertTo-SecureString -string $AdminPassword -AsPlainText -Force
   $ctx=New-Object Microsoft.SharePoint.Client.ClientContext($Url)
