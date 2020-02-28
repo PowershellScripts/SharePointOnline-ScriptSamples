8705dfc4656644d2a9389d08922483c1c61178bc
JakubPiegza <45433779+JakubPiegza@users.noreply.github.com>
2020-02-28 01:21:51 +0100

    Update GetLicensesProvisioningStatus.ps1

--- Licensing/Get user licenses and services provisioning statuses/GetLicensesProvisioningStatus.ps1
+++ Licensing/Get user licenses and services provisioning statuses/GetLicensesProvisioningStatus.ps1
@@ -17,42 +17,36 @@ Connect-MsolService
 #Gets the users
 $Users = Get-MSolUser -All  
 
-if($Users -ne $null)
-{
-Write-Host "Loaded all users."
+if($Users -ne $null){
+ Write-Host "Loaded all users."
 }
-else
-{
+else{
  return "Couldn't get the users."
 }
 
 
-if($CSVPath)
-{
+if($CSVPath){
  Write-Host "Users will be saved to" $CSVPath
 }
 
-foreach($user in $users)
-{
-  
+foreach($user in $users){
   $ss=(Get-Msoluser -UserPrincipalName $user.userPrincipalName).licenses.servicestatus
   $count=$ss.Count
   $uss=New-Object PSObject
   $uss | Add-Member -MemberType NoteProperty -Name "UPN" -Value $user.UserPrincipalName
   $uss | Add-Member -MemberType NoteProperty -Name "License" -Value (Get-Msoluser -UserPrincipalName $user.userPrincipalName).licenses.accountskuid
   $uss | Add-Member -MemberType NoteProperty -Name "Office" -Value (Get-Msoluser -UserPrincipalName $user.userPrincipalName).Office
-  for($i=0;$i -lt $count; $i++)
-  {
-  $uss | Add-Member -MemberType NoteProperty -Name $ss[$i].ServicePlan.ServiceName -Value $ss[$i].ProvisioningStatus
+  
+  for($i=0;$i -lt $count; $i++){
+   $uss | Add-Member -MemberType NoteProperty -Name $ss[$i].ServicePlan.ServiceName -Value $ss[$i].ProvisioningStatus
   }
 
   $uss
-  if($CSVPath)
-  {
-  $uss | export-csv $CSVPath -Append -Force
+  
+  if($CSVPath){
+   $uss | export-csv $CSVPath -Append -Force
   }
   #(Get-Msoluser -UserPrincipalName $user.userPrincipalName) | select UserPrincipalName, licenses | export-csv C:\Users\Arleta.Wanat\Documents\userrr4.csv -Append -Force #.servicestatus 
- 
 }
 
 
