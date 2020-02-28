6dcf37f66eff3314bad5c498ff99b83f082cfeef
JakubPiegza <45433779+JakubPiegza@users.noreply.github.com>
2020-02-28 23:01:24 +0100

    Update ReportAllPages.ps1

--- Pages/ClassicWebparts/Report on all web parts in all site pages/ReportAllPages.ps1
+++ Pages/ClassicWebparts/Report on all web parts in all site pages/ReportAllPages.ps1
@@ -1,76 +1,70 @@
 ﻿ 
- function Get-SPOWebParts
-{
-param (
-        [Parameter(Mandatory=$true,Position=1)]
+ function Get-SPOWebParts{
+	param (
+		[Parameter(Mandatory=$true,Position=1)]
 		[string]$Username,
 		[Parameter(Mandatory=$true,Position=2)]
 		[string]$Url,
-        [Parameter(Mandatory=$true,Position=3)]
+		[Parameter(Mandatory=$true,Position=3)]
 		$password,
 		[Parameter(Mandatory=$true,Position=4)]
 		[bool]$Recursive
-		)
+	)
 
 
-  $ctx=New-Object Microsoft.SharePoint.Client.ClientContext($Url)
-  $ctx.Credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($Username, $password)
-  $ctx.Load($ctx.Web)
-  $ctx.ExecuteQuery()
+	$ctx=New-Object Microsoft.SharePoint.Client.ClientContext($Url)
+	$ctx.Credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($Username, $password)
+	$ctx.Load($ctx.Web)
+	$ctx.ExecuteQuery()
 
-  $spqQuery = New-Object Microsoft.SharePoint.Client.CamlQuery
-# $spqQuery.ViewAttributes = "Scope='Recursive'"
+	$spqQuery = New-Object Microsoft.SharePoint.Client.CamlQuery
+	# $spqQuery.ViewAttributes = "Scope='Recursive'"
 
-if($Recursive)
-{
-$spqQuery.ViewXml ="<View Scope='RecursiveAll' />";
+	if($Recursive){
+		$spqQuery.ViewXml ="<View Scope='RecursiveAll' />";
+	}
+
+	$itemki=$ctx.Web.Lists.GetbyTitle("Site Pages").GetItems($spqQuery)
+	$ctx.Load($itemki)
+	$ctx.ExecuteQuery()
+
+	foreach($item in $itemki){
+		Write-Host "*****" $item.Client_Title
+		$file = $item.File
+		$ctx.Load($file)
+		$ctx.ExecuteQuery()
+		$page = $ctx.Web.GetFileByServerRelativeUrl($file.ServerRelativeUrl)
+		$wpm = $page.GetLimitedWebPartManager("Shared")
+		$ctx.Load($wpm);
+		$ctx.Load($wpm.WebParts);
+		$ctx.ExecuteQuery()
+
+		foreach($webbie in $wpm.WebParts){
+			$obj= New-Object PSObject
+			$obj | Add-Member NoteProperty -Name Page -Value $file.ServerRelativeUrl 
+			$obj | Add-Member NoteProperty -Name DefinitionID -Value $webbie.ID
+			$ctx.Load($webbie.WebPart)
+			$ctx.Load($webbie.WebPart.Properties)
+			$ctx.ExecuteQuery()
+			$obj | Add-Member NoteProperty -Name IsClosed -Value $webbie.WebPart.IsClosed
+			$obj | Add-Member NoteProperty -Name Hidden -Value $webbie.WebPart.Hidden
+			$obj | Add-Member NoteProperty -Name Subtitle -Value $webbie.WebPart.Subtitle
+			$obj | Add-Member NoteProperty -Name Title -Value $webbie.WebPart.Title
+			$obj | Add-Member NoteProperty -Name TitleUrl -Value $webbie.WebPart.TitleUrl
+			$obj | Add-Member NoteProperty -Name ZoneIndex -Value $webbie.WebPart.ZoneIndex
+			$obj | Add-Member NoteProperty -Name ServerObjectIsNull -Value $webbie.WebPart.ServerObjectIsNull
+
+			foreach($fv in $webbie.WebPart.Properties.FieldValues){
+				$obj | Add-Member NoteProperty -Name $fv -Value $fv
+			}
+
+			Write-Output $obj
+			Write-Host "-------------------------------------------------------------------------------------------"-BackgroundColor Cyan
+			Export-Csv -InputObject $obj -Append -LiteralPath C:\test634.csv
+		}
+		$page=$null
+	}
 }
-
-  $itemki=$ctx.Web.Lists.GetbyTitle("Site Pages").GetItems($spqQuery)
-  $ctx.Load($itemki)
-  $ctx.ExecuteQuery()
-  foreach($item in $itemki){
-  Write-Host "*****" $item.Client_Title
-                   $file = $item.File
-                   $ctx.Load($file)
-                   $ctx.ExecuteQuery()
-  $page = $ctx.Web.GetFileByServerRelativeUrl($file.ServerRelativeUrl)
-  $wpm = $page.GetLimitedWebPartManager("Shared")
-            $ctx.Load($wpm);
-            $ctx.Load($wpm.WebParts);
-            $ctx.ExecuteQuery()
-
-            foreach($webbie in $wpm.WebParts)
-            {
-             $obj= New-Object PSObject
-             $obj | Add-Member NoteProperty -Name Page -Value $file.ServerRelativeUrl 
-             $obj | Add-Member NoteProperty -Name DefinitionID -Value $webbie.ID
-             $ctx.Load($webbie.WebPart)
-             $ctx.Load($webbie.WebPart.Properties)
-             $ctx.ExecuteQuery()
-             $obj | Add-Member NoteProperty -Name IsClosed -Value $webbie.WebPart.IsClosed
-             $obj | Add-Member NoteProperty -Name Hidden -Value $webbie.WebPart.Hidden
-             $obj | Add-Member NoteProperty -Name Subtitle -Value $webbie.WebPart.Subtitle
-             $obj | Add-Member NoteProperty -Name Title -Value $webbie.WebPart.Title
-             $obj | Add-Member NoteProperty -Name TitleUrl -Value $webbie.WebPart.TitleUrl
-             $obj | Add-Member NoteProperty -Name ZoneIndex -Value $webbie.WebPart.ZoneIndex
-             $obj | Add-Member NoteProperty -Name ServerObjectIsNull -Value $webbie.WebPart.ServerObjectIsNull
-             
-             foreach($fv in $webbie.WebPart.Properties.FieldValues)
-             {
-              $obj | Add-Member NoteProperty -Name $fv -Value $fv
-             }
-
-             Write-Output $obj
-             Write-Host "-------------------------------------------------------------------------------------------"-BackgroundColor Cyan
-             Export-Csv -InputObject $obj -Append -LiteralPath C:\test634.csv
-
-            }
-
-            $page=$null
-            }
-
-  }
  
  
  
