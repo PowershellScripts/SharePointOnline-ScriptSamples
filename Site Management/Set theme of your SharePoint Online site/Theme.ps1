f6a6dbe1e5f89f7e83beaab23d0bb932a2267a7c
JakubPiegza <45433779+JakubPiegza@users.noreply.github.com>
2020-03-01 17:23:56 +0100

    Update Theme.ps1

--- Site Management/Set theme of your SharePoint Online site/Theme.ps1
+++ Site Management/Set theme of your SharePoint Online site/Theme.ps1
@@ -1,35 +1,32 @@
-function Set-SPOTheme
-{
-  
-   param (
-   [Parameter(Mandatory=$true,Position=1)]
+function Set-SPOTheme{
+   	param (
+		[Parameter(Mandatory=$true,Position=1)]
 		[string]$Username,
 		[Parameter(Mandatory=$true,Position=2)]
 		$AdminPassword,
-        [Parameter(Mandatory=$true,Position=3)]
+		[Parameter(Mandatory=$true,Position=3)]
 		[string]$Url,
-[Parameter(Mandatory=$true,Position=4)]
+		[Parameter(Mandatory=$true,Position=4)]
 		[string]$palette,
-[Parameter(Mandatory=$true,Position=5)]
+		[Parameter(Mandatory=$true,Position=5)]
 		[string]$font
-		)
+	)
   
-  $ctx=New-Object Microsoft.SharePoint.Client.ClientContext($Url)
-  $ctx.Credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($Username, $AdminPassword)
-  $ctx.ExecuteQuery() 
-  
-  $ctx.Load($ctx.Web)
-  $ctx.Load($ctx.Web.ThemeInfo)
-  $ctx.ExecuteQuery()
-  $palette=$ctx.web.ServerRelativeUrl+$palette
-  $font=$ctx.Web.ServerRelativeUrl+$font
-  $mynu=Out-Null
-        Write-Host "Current theme " $ctx.Web.ThemeInfo.AccessibleDescription
-        Write-Host $ctx.Web.ThemeInfo.ThemeBackgroundImageUri
-$ctx.Web.ApplyTheme($palette,$font,$mynu, $true)
-$ctx.ExecuteQuery()
-    
-     }
+	$ctx=New-Object Microsoft.SharePoint.Client.ClientContext($Url)
+	$ctx.Credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($Username, $AdminPassword)
+	$ctx.ExecuteQuery() 
+
+	$ctx.Load($ctx.Web)
+	$ctx.Load($ctx.Web.ThemeInfo)
+	$ctx.ExecuteQuery()
+	$palette=$ctx.web.ServerRelativeUrl+$palette
+	$font=$ctx.Web.ServerRelativeUrl+$font
+	$mynu=Out-Null
+	Write-Host "Current theme " $ctx.Web.ThemeInfo.AccessibleDescription
+	Write-Host $ctx.Web.ThemeInfo.ThemeBackgroundImageUri
+	$ctx.Web.ApplyTheme($palette,$font,$mynu, $true)
+	$ctx.ExecuteQuery()
+}
      
   
   
