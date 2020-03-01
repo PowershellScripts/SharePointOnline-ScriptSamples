ebba3695f871d81b4aa3b0df745816182debc5f7
JakubPiegza <45433779+JakubPiegza@users.noreply.github.com>
2020-03-01 16:54:51 +0100

    Update WhereIsMyFileWithDetails3.ps1

--- Site Management/Recycle Bin/Report on all deleted items across all site collections/WhereIsMyFileWithDetails3.ps1
+++ Site Management/Recycle Bin/Report on all deleted items across all site collections/WhereIsMyFileWithDetails3.ps1
@@ -1,60 +1,45 @@
-﻿function Get-DeletedItems
-{
-param (
-  [Parameter(Mandatory=$true,Position=1)]
+﻿function Get-DeletedItems{
+	param (
+		[Parameter(Mandatory=$true,Position=1)]
 		[string]$Username,
 		[Parameter(Mandatory=$true,Position=2)]
 		$AdminPassword,
-        [Parameter(Mandatory=$true,Position=3)]
+		[Parameter(Mandatory=$true,Position=3)]
 		[string]$Url
-)
-#$password = ConvertTo-SecureString -string $AdminPassword -AsPlainText -Force
-  $ctx=New-Object Microsoft.SharePoint.Client.ClientContext($Url)
-  $ctx.Credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($Username, $AdminPassword)
+	)
+	#$password = ConvertTo-SecureString -string $AdminPassword -AsPlainText -Force
+	$ctx=New-Object Microsoft.SharePoint.Client.ClientContext($Url)
+	$ctx.Credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($Username, $AdminPassword)
 
-try
-{
-$ctx.ExecuteQuery()
-} catch [Net.WebException] 
-        {
-            
-            Write-Host $Url " failed to connect to the site" $_.Exception.Message.ToString() -ForegroundColor Red
-}
+	try{
+		$ctx.ExecuteQuery()
+	}
+	catch [Net.WebException] {
+		Write-Host $Url " failed to connect to the site" $_.Exception.Message.ToString() -ForegroundColor Red
+	}
 
- $ctx.Load($ctx.Site)
-  $ctx.Load($ctx.Web.Webs)
-  $rb=$ctx.Site.RecycleBin
-$ctx.Load($rb)
-try
-{
-$ctx.ExecuteQuery()
-Write-Host $ctx.Site.Url $rb.Count.ToString()
-} catch [Net.WebException] 
-        {
-            
-            Write-Host $ctx.Site.Url " failed" $_.Exception.Message.ToString() -ForegroundColor Red
+	$ctx.Load($ctx.Site)
+	$ctx.Load($ctx.Web.Webs)
+	$rb=$ctx.Site.RecycleBin
+	$ctx.Load($rb)
 
-}
+	try{
+		$ctx.ExecuteQuery()
+		Write-Host $ctx.Site.Url $rb.Count.ToString()
+	}
+	catch [Net.WebException] {
+		Write-Host $ctx.Site.Url " failed" $_.Exception.Message.ToString() -ForegroundColor Red
+	}
 
-for($i=0;$i -lt $rb.Count ;$i++)
-{
-        
-        $ctx.Load($rb[$i].Author)
-        $ctx.Load($rb[$i].DeletedBy)
-        $ctx.ExecuteQuery()
-        $obj = $rb[$i]
-        $obj | Add-Member NoteProperty AuthorLoginName($rb[$i].Author.LoginName)
-        $obj | Add-Member NoteProperty DeletedByLoginName($rb[$i].DeletedBy.LoginName)
-        Write-Output $obj
-
-
-}
-
-
-
-
-
-
+	for($i=0;$i -lt $rb.Count ;$i++){      
+		$ctx.Load($rb[$i].Author)
+		$ctx.Load($rb[$i].DeletedBy)
+		$ctx.ExecuteQuery()
+		$obj = $rb[$i]
+		$obj | Add-Member NoteProperty AuthorLoginName($rb[$i].Author.LoginName)
+		$obj | Add-Member NoteProperty DeletedByLoginName($rb[$i].DeletedBy.LoginName)
+		Write-Output $obj
+	}
 }
 
 
@@ -71,11 +56,8 @@ $AdminUrl="https://tenant-admin.sharepoint.com"
 Connect-SPOService -Url $adminUrl
 $sites=(Get-SPOSite).Url
 
-foreach($site in $sites)
-{
-
- Get-DeletedItems -Username $Username -AdminPassword $AdminPassword -Url $site
-
+foreach($site in $sites){
+ 	Get-DeletedItems -Username $Username -AdminPassword $AdminPassword -Url $site
 }
 
 
