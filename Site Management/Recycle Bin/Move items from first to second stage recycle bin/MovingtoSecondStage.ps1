65351da4bc9f2d65f4e8b80efa76ece7c410d339
JakubPiegza <45433779+JakubPiegza@users.noreply.github.com>
2020-03-01 16:44:08 +0100

    Update MovingtoSecondStage.ps1

--- Site Management/Recycle Bin/Move items from first to second stage recycle bin/MovingtoSecondStage.ps1
+++ Site Management/Recycle Bin/Move items from first to second stage recycle bin/MovingtoSecondStage.ps1
@@ -1,38 +1,36 @@
 ﻿# The following script moves all items from a site collection and the specified subsite into second-stage recycle bin.
 
 
-function Move-ToSecondStage
-{
-param (
-  [Parameter(Mandatory=$true,Position=1)]
+function Move-ToSecondStage{
+	param (
+		[Parameter(Mandatory=$true,Position=1)]
 		[string]$Username,
 		[Parameter(Mandatory=$true,Position=2)]
 		$AdminPassword,
-        [Parameter(Mandatory=$true,Position=3)]
+		[Parameter(Mandatory=$true,Position=3)]
 		[string]$Url
-)
-#$password = ConvertTo-SecureString -string $AdminPassword -AsPlainText -Force
-  $ctx=New-Object Microsoft.SharePoint.Client.ClientContext($Url)
-  $ctx.Credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($Username, $AdminPassword)
-  $ctx.ExecuteQuery() 
+	)
 
- $ctx.Load($ctx.Web)
-#  $ctx.Load($ctx.Site)
-  $rb=$ctx.Web.RecycleBin
-#  $siteRB=$ctx.Site.RecycleBin
-$ctx.Load($rb)
-#$ctx.load($siteRB)
-$ctx.ExecuteQuery()
-Write-Host $ctx.Web.Url " has "  $rb.Count.ToString() " items in the recycle bin"
-#Write-Host $ctx.Site.Url " has " $SiteRB.Count.ToString() " items in the recycle bin"
+	#$password = ConvertTo-SecureString -string $AdminPassword -AsPlainText -Force
+	$ctx=New-Object Microsoft.SharePoint.Client.ClientContext($Url)
+	$ctx.Credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($Username, $AdminPassword)
+	$ctx.ExecuteQuery() 
 
-Write-Host "Moving items to the second stage recycle bin..."
-#$siteRB.MoveAllToSecondStage()
-$rb.MoveAllToSecondStage()
+	$ctx.Load($ctx.Web)
+	#  $ctx.Load($ctx.Site)
+	$rb=$ctx.Web.RecycleBin
+	#  $siteRB=$ctx.Site.RecycleBin
+	$ctx.Load($rb)
+	#$ctx.load($siteRB)
+	$ctx.ExecuteQuery()
+	Write-Host $ctx.Web.Url " has "  $rb.Count.ToString() " items in the recycle bin"
+	#Write-Host $ctx.Site.Url " has " $SiteRB.Count.ToString() " items in the recycle bin"
 
-$ctx.ExecuteQuery()
+	Write-Host "Moving items to the second stage recycle bin..."
+	#$siteRB.MoveAllToSecondStage()
+	$rb.MoveAllToSecondStage()
 
-
+	$ctx.ExecuteQuery()
 }
 
 
