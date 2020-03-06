55b585362e38fe1b1d2f8bc9d571fb6b01ba5b6a
PowershellScripts <42035526+PowershellScripts@users.noreply.github.com>
2020-03-06 19:56:04 +0200

    Add files via upload

---
+++ Tenant Settings/Globally set OneDrive for Business Access Requests and Members Can Share/ODBAccessRequests.ps1
@@ -0,0 +1,54 @@
+﻿function Set-SiteColl
+{
+param (
+        [Parameter(Mandatory=$true,Position=1)]
+		[string]$Username,
+		[Parameter(Mandatory=$true,Position=2)]
+		[string]$Url,
+        [Parameter(Mandatory=$true,Position=3)]
+		$password,
+        [Parameter(Mandatory=$true)]
+		$ODBMembersCanShare,
+        [Parameter(Mandatory=$true)]
+		$ODBAccessRequests
+		)
+
+
+  $ctx=New-Object Microsoft.SharePoint.Client.ClientContext($url)
+  $ctx.Credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($Username, $password)
+  $ctx.Load($ctx.Web)
+  $ctx.ExecuteQuery()
+
+  $spoTenant= New-Object Microsoft.Online.SharePoint.TenantAdministration.Tenant($Ctx) 
+  $ctx.Load($spoTenant)
+  $ctx.ExecuteQuery()
+  $spoTenant.ODBAccessRequests =[Microsoft.SharePoint.Client.SharingState]::$ODBAccessRequests
+  $spoTenant.ODBMembersCanShare=[Microsoft.SharePoint.Client.SharingState]::$ODBMembersCanShare
+  $ctx.Load($spoTenant)
+  $ctx.ExecuteQuery()
+  Write-Output $spoTenant  
+
+}
+
+
+
+
+#Paths to SDK
+Add-Type -Path "C:\Program Files\Common Files\Microsoft Shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.dll"
+Add-Type -Path "C:\Program Files\SharePoint Online Management Shell\Microsoft.Online.SharePoint.PowerShell\Microsoft.Online.SharePoint.Client.Tenant.dll"  
+
+
+ 
+#Enter the data
+$AdminPassword=Read-Host -Prompt "Enter password" -AsSecureString
+$username="admin@TENANT.onmicrosoft.com"
+$Url="https://TENANT-admin.sharepoint.com"
+$ODBMembersCanShare="On"  # On, Off, Unspecified
+$ODBAccessRequests="Unspecified" #On, Off, Unspecified
+
+
+
+
+
+
+Set-SiteColl -Username $username -Url $Url -password $AdminPassword -ODBMembersCanShare $ODBMembersCanShare -ODBAccessRequests $ODBAccessRequests
