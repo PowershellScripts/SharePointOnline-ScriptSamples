a1565f5aa5753111eaeb69bfba20f2cb4ceceac8
WhoeverAmI <67475706+WhoeverAmI@users.noreply.github.com>
2020-06-26 17:53:54 +0200

    formatted

--- Content Types/Content Types Management Setting/Allow content type management for all lists in a site/ContentTypeManagement.ps1
+++ Content Types/Content Types Management Setting/Allow content type management for all lists in a site/ContentTypeManagement.ps1
@@ -2,42 +2,42 @@
 # Created by Arleta Wanat, 2015 
 #
 
-function Set-SPOListsContentTypesEnabled{
-	param (
-	 	[Parameter(Mandatory=$true,Position=1)]
-		[string]$Username,
-		[Parameter(Mandatory=$true,Position=2)]
-		[string]$AdminPassword,
-		[Parameter(Mandatory=$true,Position=3)]
-		[string]$Url,
-		[Parameter(Mandatory=$true,Position=4)]
-		[bool]$ContentTypesEnabled
-	)
+function Set-SPOListsContentTypesEnabled {
+    param (
+        [Parameter(Mandatory = $true, Position = 1)]
+        [string]$Username,
+        [Parameter(Mandatory = $true, Position = 2)]
+        [string]$AdminPassword,
+        [Parameter(Mandatory = $true, Position = 3)]
+        [string]$Url,
+        [Parameter(Mandatory = $true, Position = 4)]
+        [bool]$ContentTypesEnabled
+    )
 
-  $password = ConvertTo-SecureString -string $AdminPassword -AsPlainText -Force
+    $password = ConvertTo-SecureString -string $AdminPassword -AsPlainText -Force
   
-  $ctx=New-Object Microsoft.SharePoint.Client.ClientContext($Url)
-  $ctx.Credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($Username, $password)
-  $ctx.ExecuteQuery() 
+    $ctx = New-Object Microsoft.SharePoint.Client.ClientContext($Url)
+    $ctx.Credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($Username, $password)
+    $ctx.ExecuteQuery() 
 
-  $Lists=$ctx.Web.Lists
+    $Lists = $ctx.Web.Lists
   
-  $ctx.Load($Lists)
-  $ctx.ExecuteQuery()
+    $ctx.Load($Lists)
+    $ctx.ExecuteQuery()
 
-	Foreach($ll in $Lists){
-	    $ll.ContentTypesEnabled = $ContentTypesEnabled
-	    $ll.Update()
+    Foreach ($ll in $Lists) {
+        $ll.ContentTypesEnabled = $ContentTypesEnabled
+        $ll.Update()
 
-		try{
-			$ctx.ExecuteQuery()
-			Write-Host $ll.Title "   Done" -ForegroundColor Green
-		}
-		catch [Net.WebException]{
-			Write-Host "Failed" $_.Exception.ToString() -ForegroundColor Red
-		}
+        try {
+            $ctx.ExecuteQuery()
+            Write-Host $ll.Title "   Done" -ForegroundColor Green
+        }
+        catch [Net.WebException] {
+            Write-Host "Failed" $_.Exception.ToString() -ForegroundColor Red
+        }
 
-	}
+    }
 }
 
 
@@ -47,10 +47,10 @@ Add-Type -Path "c:\Program Files\Common Files\microsoft shared\Web Server Extens
 Add-Type -Path "c:\Program Files\Common Files\microsoft shared\Web Server Extensions\15\ISAPI\Microsoft.SharePoint.Client.Runtime.dll" 
 
 # Insert the credentials and the name of the site and the desired setting: $true or $false
-$Username="trial@trialtrial123.onmicrosoft.com"
-$AdminPassword="Pass"
-$Url="https://trialtrial123.sharepoint.com/sites/teamsitewithlists"
-$ContentTypesEnabled=$true
+$Username = "trial@trialtrial123.onmicrosoft.com"
+$AdminPassword = "Pass"
+$Url = "https://trialtrial123.sharepoint.com/sites/teamsitewithlists"
+$ContentTypesEnabled = $true
 
 
 
