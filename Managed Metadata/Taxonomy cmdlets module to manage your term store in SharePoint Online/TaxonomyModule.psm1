45796ea8031848c81e75c56f79270dbd626b47e2
PowershellScripts <42035526+PowershellScripts@users.noreply.github.com>
2021-03-03 19:30:22 +0200

    Update TaxonomyModule.psm1

--- Managed Metadata/Taxonomy cmdlets module to manage your term store in SharePoint Online/TaxonomyModule.psm1
+++ Managed Metadata/Taxonomy cmdlets module to manage your term store in SharePoint Online/TaxonomyModule.psm1
@@ -381,4 +381,3 @@ $global:ctx
 Add-Type -Path "c:\Program Files\Common Files\microsoft shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.dll" 
 Add-Type -Path "c:\Program Files\Common Files\microsoft shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.Runtime.dll" 
 Add-Type -Path "c:\Program Files\Common Files\microsoft shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.Taxonomy.dll" 
-
