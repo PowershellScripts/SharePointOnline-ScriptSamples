b4114b1129c26b3e0071f3a2f9377719873e0c74
PowershellScripts <42035526+PowershellScripts@users.noreply.github.com>
2021-12-04 19:06:00 +0200

    Update Create empty node with PnP.ps1

--- Site Management/Navigation/Create empty node (label)/Create empty node with PnP.ps1
+++ Site Management/Navigation/Create empty node (label)/Create empty node with PnP.ps1
@@ -1,1 +1,22 @@
-..
+$NavNodeTitle = "Header"
+
+Connect-PnPOnline
+
+
+## SharePoint Online
+
+# add an empty link (header) to quick launch on SharePoint Online site 
+Add-PnPNavigationNode -Title $NavNodeTitle -Location QuickLaunch -Url "http://linkless.header/"
+
+# add an empty link (header) to top navigation on SharePoint Online site 
+Add-PnPNavigationNode -Title $NavNodeTitle -Location TopNavigationBar -Url "http://linkless.header/"
+
+
+
+## SharePoint Server 2016
+
+# add an empty link (header) to quick launch on SharePoint Server 2016 site
+Add-PnPNavigationNode -Title $NavNodeTitle -Location QuickLaunch -Url ""
+
+# add an empty link (header) to top navigation on SharePoint Server 2016 site
+Add-PnPNavigationNode -Title $NavNodeTitle -Location TopNavigationBar -Url ""
