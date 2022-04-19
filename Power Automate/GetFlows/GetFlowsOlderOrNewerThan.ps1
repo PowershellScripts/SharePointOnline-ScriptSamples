e584718cda4d79f85194a595fb144c624ca4f203
Arleta Wanat <42035526+PowershellScripts@users.noreply.github.com>
2022-04-19 21:50:45 +0200

    Update GetFlowsOlderOrNewerThan.ps1

--- Power Automate/GetFlows/GetFlowsOlderOrNewerThan.ps1
+++ Power Automate/GetFlows/GetFlowsOlderOrNewerThan.ps1
@@ -1,1 +1,11 @@
+This file contains a few examples. Each of them can be executed separately
 
+
+
+# Get flows older than a year
+
+Connect-PnPOnline
+$environment = Get-PnPPowerPlatformEnvironment
+$CompareDate = (Get-Date).AddYears(-1)
+
+Get-PnPFlow -Environment $environment | select -ExpandProperty Properties | Where-Object {$_.CreatedTime -gt $CompareDate}
