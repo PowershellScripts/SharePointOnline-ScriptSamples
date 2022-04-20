eeb2360b35fc2cbd573a2037c7725dc12ff84494
Arleta Wanat <42035526+PowershellScripts@users.noreply.github.com>
2022-04-20 10:52:02 +0200

    Update GetFlowsOlderOrNewerThan.ps1

--- Power Automate/GetFlows/GetFlowsOlderOrNewerThan.ps1
+++ Power Automate/GetFlows/GetFlowsOlderOrNewerThan.ps1
@@ -1,4 +1,5 @@
-This file contains a few examples. Each of them can be executed separately
+# This file contains a few examples. Each of them can be executed separately.
+# Mind that when talking about flows older or newer than, the created time is considered, not the last time they were in use.
 
 
 
@@ -8,4 +9,36 @@ Connect-PnPOnline
 $environment = Get-PnPPowerPlatformEnvironment
 $CompareDate = (Get-Date).AddYears(-1)
 
-Get-PnPFlow -Environment $environment | select -ExpandProperty Properties | Where-Object {$_.CreatedTime -gt $CompareDate}
+Get-PnPFlow -AsAdmin -Environment $environment | select -ExpandProperty Properties | Where-Object {$_.CreatedTime -lt $CompareDate}
+
+
+
+
+
+
+# Export flows older than a year to a CSV file
+
+$CSVPath = "D:\HereIStoreMyData\Flows.csv"
+
+Connect-PnPOnline
+$environment = Get-PnPPowerPlatformEnvironment
+$CompareDate = (Get-Date).AddYears(-1)
+
+Get-PnPFlow -AsAdmin -Environment $environment | select -ExpandProperty Properties | Where-Object {$_.CreatedTime -lt $CompareDate} | Export-CSV -Path $CSVPath -Append
+
+
+
+
+
+
+# Export flows newer than a month to a CSV file
+
+$CSVPath = "D:\HereIStoreMyData\Flows.csv"
+
+Connect-PnPOnline
+$environment = Get-PnPPowerPlatformEnvironment
+$CompareDate = (Get-Date).AddMonths(-1)
+
+Get-PnPFlow -AsAdmin -Environment $environment | select -ExpandProperty Properties | Where-Object {$_.CreatedTime -gt $CompareDate} | Export-CSV -Path $CSVPath -Append
+
+
