e100579dca7f04207cd7e7d52c18b8c51d8601da
Arleta Wanat <42035526+PowershellScripts@users.noreply.github.com>
2023-12-16 21:44:02 +0100

    Create ScriptUpdated2023.ps1

---
+++ Content Types/Content Types Management Setting/Set content type management setting for a single list/ScriptUpdated2023.ps1
@@ -0,0 +1,36 @@
+﻿
+#
+# Created by Arleta Wanat, 2023
+#
+
+
+# Paths to SDK. Please verify location on your computer.
+Add-Type -Path "c:\Program Files\Common Files\microsoft shared\Web Server Extensions\15\ISAPI\Microsoft.SharePoint.Client.dll" 
+Add-Type -Path "c:\Program Files\Common Files\microsoft shared\Web Server Extensions\15\ISAPI\Microsoft.SharePoint.Client.Runtime.dll" 
+
+# Insert the credentials and the name of the site and list
+$Username="trial@trialtrial123.onmicrosoft.com"
+$AdminPassword="Pass"
+$Url="https://trialtrial123.sharepoint.com/sites/teamsitewithlists"
+$ListName="Tasks list"
+$ContentTypesEnabled =$false   # change it to true if you want to enable the setting
+
+# Connect
+	$password = ConvertTo-SecureString -string $AdminPassword -AsPlainText -Force
+  $ctx=New-Object Microsoft.SharePoint.Client.ClientContext($Url)
+	$ctx.Credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($Username, $password)
+	$ctx.ExecuteQuery() 
+
+# Update the setting
+
+$ll=$ctx.Web.Lists.GetByTitle($ListName)
+$ll.ContentTypesEnabled = $ContentTypesEnabled
+$ll.Update()
+
+try{
+		$ctx.ExecuteQuery()
+		Write-Host "Done" -ForegroundColor Green
+	}
+catch(){
+		Write-Host "Failed" $_.Exception.ToString() -ForegroundColor Red
+	}
