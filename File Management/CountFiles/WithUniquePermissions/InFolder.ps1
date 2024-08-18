a3d88aa467be687b8c0bcace819d565178be47ce
Arleta Wanat <42035526+PowershellScripts@users.noreply.github.com>
2024-08-18 17:47:33 +0200

    Update InFolder.ps1

--- File Management/CountFiles/WithUniquePermissions/InFolder.ps1
+++ File Management/CountFiles/WithUniquePermissions/InFolder.ps1
@@ -1,1 +1,24 @@
-.
+
+# Connect to SharePoint Online
+Connect-PnPOnline -Url "https://yoursite.sharepoint.com/sites/yoursite" -Interactive
+
+# Define the folder URL relative to the document library
+$folderUrl = "/sites/yoursite/Shared Documents/YourFolder"
+$list = "Shared Documents"
+
+# Initialize the count for items with unique permissions
+$fileCountWithUniquePermissions = 0;
+
+# Get the folder
+$folder = Get-PnPFolder -Url $folderUrl -Includes Files, Folders
+
+foreach ($file in $folder.Files) {
+    $fileItem = Get-PnPListItem -List $list -Id $file.ListItemAllFields.Id
+    if ($fileItem.HasUniqueRoleAssignments) {
+        $fileCountWithUniquePermissions++
+    }
+}
+
+
+# Output the total count
+Write-Host "Total files:  $fileCountWithUniquePermissions"
