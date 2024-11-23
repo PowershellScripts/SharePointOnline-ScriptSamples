d55bd5a44de156e5f8c75a07fc65d6f5da85ac86
Arleta Wanat <42035526+PowershellScripts@users.noreply.github.com>
2024-11-23 12:24:21 +0100

    Update compare2tenantsCSOM.ps1

--- Tenant Settings/Compare2tenants/compare2tenantsCSOM.ps1
+++ Tenant Settings/Compare2tenants/compare2tenantsCSOM.ps1
@@ -1,3 +1,9 @@
+# Written 2024 by Arleta Wanat
+# The script looks for differences between SharePoint settings in 2 Microsoft 365 tenants. It displays the differences and on the screen and generates 2 files:
+# One CSV only with differences, and one CSV with all the compared settings
+# For more info go to https://powershellscripts.github.io/articles/en/spo/comparetenants
+
+
 # Load required assemblies
 Add-Type -Path "C:\Program Files\Common Files\microsoft shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.dll"
 Add-Type -Path "C:\Program Files\Common Files\microsoft shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.Runtime.dll"
